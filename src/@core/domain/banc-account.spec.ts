b8a4f9e2ab3ca1a883eb91ec44f6fe147fcaa06c
Dikendev <diken.dev@gmail.com>
2023-06-22 23:42:40 -0300

    bank account test

---
+++ src/@core/domain/banc-account.spec.ts
@@ -0,0 +1,19 @@
+import { BankAccount } from "./bank-account"
+
+describe('BankAccount Unit Test', () => {
+
+  it('should create a bank account',() => {
+    const bankAccount = new BankAccount('123a',300,'33333');
+
+    expect(bankAccount.id).toBe('123a');
+    expect(bankAccount.balance).toBe(300),
+    expect(bankAccount.account_number).toBe('33333');
+  })
+
+  it('should debit an account', () => {
+    const bankAccount = new BankAccount('123a',300,'33333');
+    bankAccount.debit(100);
+
+    expect(bankAccount.balance).toBe(200);
+  })
+})
